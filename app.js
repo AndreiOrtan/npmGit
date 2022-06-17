8f6b182c1b3df7fbbfb322469b510a83ac5c132f
ortanandrei <ortanandrei2000@gmail.com>
2022-06-17 22:42:24 +0300

    Added src and lib forlders

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-var moment = require("moment");
-var unique = require("uniq");
-
-var nums = [1, 1, 1, 1, 1, 22, 2, 33, 3, 66, 5, 6];
-console.log(unique(nums));
-
-const date = new Date();
-const dateNew = moment(date).format("LL");
-console.log(dateNew);
+let add = (num1, num2) => num1 + num2;
