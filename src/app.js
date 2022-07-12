e2f9f9a8f2fe3f7e6cec33b87cae29169ad6012f
ortanandrei <ortanandrei2000@gmail.com>
2022-07-12 23:16:36 +0300

    Added mock companies to work with

--- src/app.js
+++ src/app.js
@@ -1,40 +1,14 @@
 import createHeader from "./components/header/Header";
 import StockBanner from "./components/section/StockBanner";
 import Layout from "./components/layout/Layout.js";
-
-const companies = [
-  {
-    company: "Apple Inc",
-    companyInfo: "Apple has the best software.",
-    currentPrice: 311.58,
-    oldPrice: 289.44,
-  },
-  {
-    company: "Amazon Inc",
-    companyInfo: "Amazon.com, Inc. is an American company.",
-    currentPrice: 271.58,
-    oldPrice: 289.44,
-  },
-  {
-    company: "Google LLC",
-    companyInfo:
-      "Google LLC is a company that focuses on artificial intelligence",
-    currentPrice: 211.58,
-    oldPrice: 189.44,
-  },
-  {
-    company: "Facebook Inc",
-    companyInfo: "Facebook is an American multinational",
-    currentPrice: 451.58,
-    oldPrice: 439.44,
-  },
-];
+import companies from "../data/mockCompanies";
 
 function app(element) {
   element.appendChild(createHeader());
-  for (const company of companies) {
+
+  companies.forEach((company) => {
     Layout.appendChild(StockBanner(company));
-  }
+  });
 
   element.appendChild(Layout);
 }
--- src/components/section/StockBanner.js
+++ src/components/section/StockBanner.js
@@ -2,14 +2,12 @@ import gen from "../../../lib/generateElement/generateElement";
 import "./StockBanner.css";
 
 function createSection(props) {
-  function isWhatPercentOf(num1, num2) {
-    return (num1 / num2) * 100;
-  }
-  const percent = (
-    100 - isWhatPercentOf(props.oldPrice, props.currentPrice)
-  ).toFixed(2);
+  const { company, companyInfo, currentPrice, oldPrice } = props;
+  const percent = (100 - (props.oldPrice / props.currentPrice) * 100).toFixed(
+    2
+  );
 
-  const priceDiff = (props.currentPrice - props.oldPrice).toFixed(2);
+  const priceDiff = (currentPrice - oldPrice).toFixed(2);
 
   let operator = "";
   if (props.currentPrice > props.oldPrice) {
@@ -18,11 +16,11 @@ function createSection(props) {
 
   return gen("section", { class: "row card-panel" }, [
     gen("div", { class: "col s12 m4 l6" }, [
-      gen("div", { class: "company" }, [props.company]),
-      gen("div", { class: "company-info" }, [props.companyInfo]),
+      gen("div", { class: "company" }, [company]),
+      gen("div", { class: "company-info" }, [companyInfo]),
     ]),
     gen("div", { class: "col s12 m4 l6 right-align" }, [
-      gen("div", { class: "current-price" }, ["$" + props.currentPrice]),
+      gen("div", { class: "current-price" }, ["$" + currentPrice]),
       gen(
         "div",
         {
