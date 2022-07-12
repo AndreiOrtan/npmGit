3da2909b812a7d6d2ba5a9eab16ed0ddd87f1eda
ortanandrei <ortanandrei2000@gmail.com>
2022-07-12 14:58:55 +0300

    Added companies array and managed negative values

--- src/app.js
+++ src/app.js
@@ -2,16 +2,40 @@ import createHeader from "./components/header/Header";
 import StockBanner from "./components/section/StockBanner";
 import Layout from "./components/layout/Layout.js";
 
+const companies = [
+  {
+    company: "Apple Inc",
+    companyInfo: "Apple has the best software.",
+    currentPrice: 311.58,
+    oldPrice: 289.44,
+  },
+  {
+    company: "Amazon Inc",
+    companyInfo: "Amazon.com, Inc. is an American company.",
+    currentPrice: 271.58,
+    oldPrice: 289.44,
+  },
+  {
+    company: "Google LLC",
+    companyInfo:
+      "Google LLC is a company that focuses on artificial intelligence",
+    currentPrice: 211.58,
+    oldPrice: 189.44,
+  },
+  {
+    company: "Facebook Inc",
+    companyInfo: "Facebook is an American multinational",
+    currentPrice: 451.58,
+    oldPrice: 439.44,
+  },
+];
+
 function app(element) {
   element.appendChild(createHeader());
-  Layout.appendChild(
-    StockBanner({
-      company: "Apple Inc",
-      companyInfo: "Apple has the best software.",
-      currentPrice: 191.58,
-      oldPrice: 183.44,
-    })
-  );
+  for (const company of companies) {
+    Layout.appendChild(StockBanner(company));
+  }
+
   element.appendChild(Layout);
 }
 
--- src/components/section/StockBanner.js
+++ src/components/section/StockBanner.js
@@ -5,6 +5,17 @@ function createSection(props) {
   function isWhatPercentOf(num1, num2) {
     return (num1 / num2) * 100;
   }
+  const percent = (
+    100 - isWhatPercentOf(props.oldPrice, props.currentPrice)
+  ).toFixed(2);
+
+  const priceDiff = (props.currentPrice - props.oldPrice).toFixed(2);
+
+  let operator = "";
+  if (props.currentPrice > props.oldPrice) {
+    operator = "+";
+  }
+
   return gen("section", { class: "row card-panel" }, [
     gen("div", { class: "col s12 m4 l6" }, [
       gen("div", { class: "company" }, [props.company]),
@@ -12,13 +23,14 @@ function createSection(props) {
     ]),
     gen("div", { class: "col s12 m4 l6 right-align" }, [
       gen("div", { class: "current-price" }, ["$" + props.currentPrice]),
-      gen("div", { class: "old-price" }, [
-        `\$${(props.currentPrice - props.oldPrice).toFixed(2)}`,
-
-        ` (+${(
-          100 - isWhatPercentOf(props.oldPrice, props.currentPrice)
-        ).toFixed(2)}%)`,
-      ]),
+      gen(
+        "div",
+        {
+          class: "old-price",
+          style: priceDiff < 0 ? "color:red" : "color:green",
+        },
+        [`\$${priceDiff}(${operator}${percent}%)`]
+      ),
       gen("div", { class: "current-date" }, [`${new Date()}`]),
     ]),
   ]);
