a9dff8514e50d95d70c19e79c4970e500e3cee87
ortanandrei <ortanandrei2000@gmail.com>
2022-07-21 12:58:43 +0300

    Added highlight on click

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import createHeader from "./components/header/Header";
 import StockBanner from "./components/section/StockBanner";
 import Layout from "./components/layout/Layout.js";
 import companies from "../data/mockCompanies";
+import "./app.css";
 
 function app(element) {
   element.appendChild(createHeader());
@@ -11,6 +12,14 @@ function app(element) {
   });
 
   element.appendChild(Layout);
+
+  Layout.addEventListener("click", (e) => {
+    const sections = Layout.querySelectorAll("section");
+    for (const section of sections) {
+      section.classList.remove("highlight");
+    }
+    e.target.closest("section").classList.add("highlight");
+  });
 }
 
 export default app;
